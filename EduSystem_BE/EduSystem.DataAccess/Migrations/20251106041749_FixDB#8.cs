b025d8b037d4b8af783533c97b5db0b7ebb073e6
LongTran2003 <quylongtranngoc07@gmail.com>
2025-11-06 11:20:44 +0700

    FixDb#8

---
+++ EduSystem_BE/EduSystem.DataAccess/Migrations/20251106041749_FixDB#8.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduSystem.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDB8 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "QuestionType",
+                table: "Questions",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Level",
+                table: "Questions",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Questions",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "EduSystem-Admin",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "601130b9-353a-4cb2-afd9-34dc32e0c66e", "AQAAAAIAAYagAAAAEOIQPomLCHoh6oODOMJYGHf+zS3rTHpItkW78vNIyufBIYwdzpmmXz4txVR/ET0jUw==", "b66cfb94-7e1f-4b3d-881f-34e3f463a741" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Questions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuestionType",
+                table: "Questions",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Level",
+                table: "Questions",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "EduSystem-Admin",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "5132d498-aa12-41a0-9569-571b4be1fbde", "AQAAAAIAAYagAAAAEOpoBHe1vho0jjTA9qQ9M4ndJYTHSGHM05YNamAnO1JqIzzw/egWj6jSI9falog8ow==", "d7d5986c-4b6e-4932-bdc1-e99dc36b7b91" });
+        }
+    }
+}
